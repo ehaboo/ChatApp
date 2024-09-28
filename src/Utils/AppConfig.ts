7abe82dd838a73f1181581e0b0046076e9ad67db
Ehab Abu Hadwan <128914180+ehaboo@users.noreply.github.com>
2024-09-28 21:32:38 +0300

    Update AppConfig.ts

--- src/Utils/AppConfig.ts
+++ src/Utils/AppConfig.ts
@@ -1,7 +1,7 @@
 
 class AppConfig {
-    public projectId = "c8122411-617e-40d1-9a97-6ef8a95255b7";
-    public privateKey = "08d0dd24-c81b-4435-8e8a-1816ad2ff1b7";
+    public projectId = "";
+    public privateKey = "";
     public createChat = "https://api.chatengine.io/users/me/";
     public createUser = "https://api.chatengine.io/users/"; 
     public createFirstChat = "https://api.chatengine.io/chats/";
